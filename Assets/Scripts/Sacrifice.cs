b23b8287fc9dfdd2de5b1753d1afed1a397c9352
Kwinten Van den berghe <Kwinten.VandenBerghe@cloudflight.io>
2020-04-30 19:20:12 +0200

    Basic Sacrifice prefab

---
+++ Assets/Scripts/Sacrifice.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Sacrifice : MonoBehaviour
+{
+    // each sacrifice needs some stats:
+    // * god favor modifier
+    // * people favor modifier
+    // * name
+    // * flavor text? / description
+    // * current position in queue
+
+    [SerializeField] private string sacrificeName;
+    [SerializeField] private string description;
+    [SerializeField] private float godFavorModifier;
+    [SerializeField] private float peopleFavorModifier;
+    [SerializeField] private int queuePosition;
+
+    private void Update()
+    {
+        // some queue movement code in here maybe
+    }
+}
