80392cb92d34d1710fde75705ba59043e8471a36
Kwinten Van den berghe <Kwinten.VandenBerghe@cloudflight.io>
2020-05-01 20:03:44 +0200

    UI changes (properly show king happiness)

--- Assets/Scripts/UI/FavorInfoPanel.cs
+++ Assets/Scripts/UI/FavorInfoPanel.cs
@@ -4,10 +4,12 @@ using UnityEngine.UI;
 public class FavorInfoPanel : MonoBehaviour
 {
     [SerializeField] private Image kingFavorFill;
+    [SerializeField] private PointSystem pointSystem;
 
     private void Awake()
     {
         Game.Instance.PointsUpdated += OnPointsUpdated;
+        OnPointsUpdated(pointSystem.KingFavor);
     }
 
     private void OnPointsUpdated(float kingFavor)
