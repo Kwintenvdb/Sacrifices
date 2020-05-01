840ae43f39f97cddf57c105c83f1ae13d5b7aecc
Kwinten Van den berghe <Kwinten.VandenBerghe@cloudflight.io>
2020-05-01 13:02:04 +0200

    Added end game scene

---
+++ Assets/Scripts/UI/EndGameScreen.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGameScreen : MonoBehaviour
+{
+    // TODO
+    // Fetch information from Game instance
+    // Display information in scene / spawn objects as needed
+
+    public void RestartGame()
+    {
+        Destroy(Game.Instance.gameObject);
+        SceneManager.LoadScene(0);
+    }
+}
