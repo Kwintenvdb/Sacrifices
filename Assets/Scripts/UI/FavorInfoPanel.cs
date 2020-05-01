66ad86d298cec7d0abce5e5a1583bee0303aaa8b
Kwinten Van den berghe <Kwinten.VandenBerghe@cloudflight.io>
2020-05-01 17:06:51 +0200

    Changed UI

--- Assets/Scripts/UI/FavorInfoPanel.cs
+++ Assets/Scripts/UI/FavorInfoPanel.cs
@@ -4,7 +4,6 @@ using UnityEngine.UI;
 public class FavorInfoPanel : MonoBehaviour
 {
     [SerializeField] private Image kingFavorFill;
-    [SerializeField] private Image peopleFavorFill;
 
     private void Awake()
     {
