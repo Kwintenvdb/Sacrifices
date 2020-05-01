41746e5f5ca2d9a656af493a775fe28b7cc886ab
Kwinten Van den berghe <Kwinten.VandenBerghe@cloudflight.io>
2020-05-01 14:21:13 +0200

    Added screams to throws

--- Assets/Scripts/Audio/AudioController.cs
+++ Assets/Scripts/Audio/AudioController.cs
@@ -4,13 +4,36 @@ public class AudioController : MonoBehaviour
 {
     [SerializeField] private AudioSource drumRollAudio;
 
+    [SerializeField] private AudioSource manScream;
+    [SerializeField] private AudioSource womanScream;
+    [SerializeField] private AudioSource wilhelmScream;
+
     private void Awake()
     {
         Game.Instance.SacrificeDraggingStarted += OnDraggingStarted;
+        Game.Instance.SacrificeThrown += OnSacrificeThrown;
+    }
+
+    private void OnSacrificeThrown(Sacrifice sacrifice)
+    {
+        switch (sacrifice.Data.type)
+        {
+            case SacrificeType.Male:
+                manScream.Play();
+                break;
+            case SacrificeType.Female:
+                womanScream.Play();
+                break;
+            case SacrificeType.Wilhelm:
+                wilhelmScream.Play();
+                break;
+        }
     }
 
     private void OnDraggingStarted(Sacrifice sacrifice)
     {
         drumRollAudio.Play();
     }
+
+
 }
