0891ab37b04532b2362d906ad324d791ab15d550
Kwinten Van den berghe <Kwinten.VandenBerghe@cloudflight.io>
2020-05-01 10:33:54 +0200

    Added audio controller

---
+++ Assets/Scripts/Audio/AudioController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    [SerializeField] private AudioSource drumRollAudio;
+
+    private void Awake()
+    {
+        Game.Instance.SacrificeDraggingStarted += OnDraggingStarted;
+    }
+
+    private void OnDraggingStarted(Sacrifice sacrifice)
+    {
+        drumRollAudio.Play();
+    }
+}
