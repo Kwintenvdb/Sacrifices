824fcb2945b6f565c80187971f1213160b1f61ef
Kwinten Van den berghe <Kwinten.VandenBerghe@cloudflight.io>
2020-05-02 12:52:46 +0200

    Added clouds, prefabs

---
+++ Assets/Scripts/Effects/Cloud.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Cloud : MonoBehaviour
+{
+    [SerializeField] float speed = 1;
+
+    private void Update()
+    {
+        var pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+    }
+}
---
+++ Assets/Scripts/Effects/CloudEmitter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudEmitter : MonoBehaviour
+{
+    [SerializeField] private float cloudSpawnTime = 10f;
+    [SerializeField] private float cloudYPositionRange = 5f;
+    [SerializeField] private List<GameObject> cloudPrefabs;
+    [SerializeField] private Transform cloudSpawnLocation;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnClouds());
+    }
+
+    private IEnumerator SpawnClouds()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(cloudSpawnTime);
+            SpawnCloud();
+        }
+    }
+
+    private void SpawnCloud()
+    {
+        var cloudIndex = Random.Range(0, cloudPrefabs.Count);
+        var prefab = cloudPrefabs[cloudIndex];
+        var pos = cloudSpawnLocation.position;
+        pos.y += Random.Range(-cloudYPositionRange / 2f, cloudYPositionRange / 2f);
+        Instantiate(prefab, pos, Quaternion.identity);
+    }
+}
